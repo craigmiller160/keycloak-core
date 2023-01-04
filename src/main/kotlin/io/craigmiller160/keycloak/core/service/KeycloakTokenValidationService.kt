f05b11819d736d8bbf5a5971ee47050f88a4e5d4
Craig Miller <craigmiller160@gmail.com>
2023-01-04 17:50:29 -0500

    A few tweaks

--- src/main/kotlin/io/craigmiller160/keycloak/core/service/KeycloakTokenValidationService.kt
+++ src/main/kotlin/io/craigmiller160/keycloak/core/service/KeycloakTokenValidationService.kt
@@ -5,8 +5,9 @@ import io.craigmiller160.keycloak.core.config.KeycloakConfig
 import io.craigmiller160.keycloak.core.function.TryEither
 
 class KeycloakTokenValidationService(
-    private val config: KeycloakConfig
+    config: KeycloakConfig
 ) {
+    // TODO not inject-able, so cannot be tested
     private val jwkSet: TryEither<JWKSet> = KeycloakOperationService.getJWKSet(config)
 
     fun validateToken(token: String) {
