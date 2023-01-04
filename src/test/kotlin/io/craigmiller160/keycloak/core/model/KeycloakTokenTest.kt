4ed9ef01f69c455ff5afd8db0a6a86ba152369ea
Craig Miller <craigmiller160@gmail.com>
2023-01-04 17:49:19 -0500

    Stubbed out test

---
+++ src/test/kotlin/io/craigmiller160/keycloak/core/model/KeycloakTokenTest.kt
@@ -0,0 +1,10 @@
+package io.craigmiller160.keycloak.core.model
+
+import org.junit.jupiter.api.Test
+
+class KeycloakTokenTest {
+    @Test
+    fun `converts claims to token object`() {
+        TODO()
+    }
+}
