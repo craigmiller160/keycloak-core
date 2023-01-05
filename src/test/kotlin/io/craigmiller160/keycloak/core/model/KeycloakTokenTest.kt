b8cc8d17ce0a69b3626c29e4fb70ce6dc293a5a0
Craig Miller <craigmiller160@gmail.com>
2023-01-05 17:29:21 -0500

    Progress

--- src/test/kotlin/io/craigmiller160/keycloak/core/model/KeycloakTokenTest.kt
+++ src/test/kotlin/io/craigmiller160/keycloak/core/model/KeycloakTokenTest.kt
@@ -1,11 +1,22 @@
 package io.craigmiller160.keycloak.core.model
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
+import com.nimbusds.jwt.JWTClaimsSet
+import io.craigmiller160.keycloak.core.model.keycloak.KeycloakToken
 import org.junit.jupiter.api.Test
 
 class KeycloakTokenTest {
-  private val objectMapper = ObjectMapper()
   @Test
   fun `converts claims to token object`() {
-    TODO()
+    val rawClaimsType = jacksonTypeRef<Map<String, Any>>()
+    val rawClaims =
+      KeycloakTokenTest::class.java.classLoader.getResourceAsStream("keycloak-token.json").let {
+        jacksonObjectMapper().readValue(it, rawClaimsType)
+      }
+    val claimsSet = JWTClaimsSet.parse(rawClaims)
+
+    val token = KeycloakToken.fromClaimsSet(claimsSet)
+    TODO("Validate the token")
   }
 }
