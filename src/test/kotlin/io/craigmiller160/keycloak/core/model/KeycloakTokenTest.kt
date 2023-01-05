36606ce715d11eee4e0b50eb404f53fbae26a419
Craig Miller <craigmiller160@gmail.com>
2023-01-05 17:26:40 -0500

    Got jackson dependencies working

--- src/test/kotlin/io/craigmiller160/keycloak/core/model/KeycloakTokenTest.kt
+++ src/test/kotlin/io/craigmiller160/keycloak/core/model/KeycloakTokenTest.kt
@@ -1,12 +1,9 @@
 package io.craigmiller160.keycloak.core.model
 
-import java.util.UUID
 import org.junit.jupiter.api.Test
 
 class KeycloakTokenTest {
-  private val rawClaims =
-    mapOf<String, Any>(
-      "exp" to 1, "iat" to 2, "auth_time" to 3, "jti" to UUID.randomUUID().toString())
+  private val objectMapper = ObjectMapper()
   @Test
   fun `converts claims to token object`() {
     TODO()
