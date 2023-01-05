50ac674cefe5698fb454a20c5f7349a0c5bf6726
Craig Miller <craigmiller160@gmail.com>
2023-01-04 20:44:22 -0500

    Refactored logic

--- src/main/kotlin/io/craigmiller160/keycloak/core/service/KeycloakTokenValidationService.kt
+++ src/main/kotlin/io/craigmiller160/keycloak/core/service/KeycloakTokenValidationService.kt
@@ -7,8 +7,10 @@ import com.nimbusds.jose.jwk.JWKSet
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet
 import com.nimbusds.jose.proc.JWSVerificationKeySelector
 import com.nimbusds.jose.proc.SecurityContext
+import com.nimbusds.jwt.JWT
 import com.nimbusds.jwt.SignedJWT
 import com.nimbusds.jwt.proc.DefaultJWTProcessor
+import com.nimbusds.jwt.proc.JWTProcessor
 import io.craigmiller160.keycloak.core.config.KeycloakConfig
 import io.craigmiller160.keycloak.core.function.TryEither
 import io.craigmiller160.keycloak.core.function.flatMapCatch
@@ -17,7 +19,7 @@ class KeycloakTokenValidationService(
     config: KeycloakConfig,
     operationsService: KeycloakOperationService
 ) {
-    // TODO not inject-able, so cannot be tested
+    // TODO not efficient
     private val jwkSetEither: TryEither<JWKSet> = operationsService.getJWKSet(config)
 
     fun validateToken(token: String) {
@@ -26,13 +28,15 @@ class KeycloakTokenValidationService(
             val jwkSet = jwkSetEither.bind()
             Pair(jwt, jwkSet)
         }
-            .map { (jwt, jwkSet) ->
-                val jwtProcessor = DefaultJWTProcessor<SecurityContext>()
-                val keySource = ImmutableJWKSet<SecurityContext>(jwkSet)
-                val keySelector = JWSVerificationKeySelector(jwt.header.algorithm, keySource)
-                jwtProcessor.jwsKeySelector = keySelector
-                Pair(jwt, jwtProcessor)
-            }
+            .map { (jwt, jwkSet) -> jwt to createJwtProcessor(jwt, jwkSet) }
             .flatMapCatch { (jwt, jwtProcessor) -> jwtProcessor.process(jwt, null) }
+    }
+
+    private fun createJwtProcessor(jwt: SignedJWT, jwkSet: JWKSet): JWTProcessor<SecurityContext> {
+        val keySource = ImmutableJWKSet<SecurityContext>(jwkSet)
+        val keySelector = JWSVerificationKeySelector(jwt.header.algorithm, keySource)
+        return DefaultJWTProcessor<SecurityContext>().apply {
+            jwsKeySelector = keySelector
+        }
     }
 }
